f14350c3d0927b3ba949b145e0744a27aa2d9d81
dipendradangol <dipendra.dangol11@gmail.com>
2021-01-17 00:17:08 +0200

    setup and initial commit

---
+++ src/components/Products/Product/Product.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+import classes from '*.module.css';
+
+// destructure product from the props
+const Product = ({ product }) => {
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image='' title={Product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {Product.name}
+                    </Typography> 
+                    <Typography variant="h5" >
+                        {Product.price}
+                    </Typography> 
+                </div>
+                    <Typography variant="h2" color="textSecondary">{Product.description}</Typography> 
+            </CardContent>
+            <CardActions disableSpacing className={classes.CardActions}>
+                <IconButton aria-label="Add to Cart">
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default  Product;
