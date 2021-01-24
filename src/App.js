d06d09bf2dc5dcd9a0d47c9828f1bbe8c244a432
dipendradangol <dipendra.dangol11@gmail.com>
2021-01-24 19:59:21 +0200

    Navbar and commerce products from commerce.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+
+import { Products, Navbar } from './components';
 
 const App = () =>  {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div>
-      <h1>e-commerce</h1>
+      <Navbar />
+      <Products products={products} />
     </div>
   )
 }
--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
-import classes from '*.module.css';
 
-// destructure product from the props
+import useStyles from './styles';
+
+
 const Product = ({ product }) => {
+    const classes = useStyles();
+
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image='' title={Product.name} />
+            <CardMedia className={classes.media} image={product.media.source} title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant="h5" gutterBottom>
-                        {Product.name}
+                        {product.name}
                     </Typography> 
                     <Typography variant="h5" >
-                        {Product.price}
+                        {product.price.formatted_with_symbol}
                     </Typography> 
                 </div>
-                    <Typography variant="h2" color="textSecondary">{Product.description}</Typography> 
+                    <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" /> 
             </CardContent>
             <CardActions disableSpacing className={classes.CardActions}>
                 <IconButton aria-label="Add to Cart">
