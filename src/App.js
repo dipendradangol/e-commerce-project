0e5c72d45f0a792a4d9b08e8e3ce2a2e967e24a1
dipendradangol <dipendra.dangol11@gmail.com>
2021-02-05 13:04:12 +0200

    made buttons functional to increment, decrement, remove from cart

--- src/App.js
+++ src/App.js
@@ -20,8 +20,22 @@ const App = () =>  {
   }
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart)
+    const response = await commerce.cart.add(productId, quantity);
+    setCart(response.cart);
+  }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const response = await commerce.cart.update(productId, { quantity });
+    setCart(response.cart);
+  }
+
+  const handleRemoveFromCart = async (productId) => {
+    const response = await commerce.cart.remove(productId);
+    setCart(response.cart);
+  }
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
+    setCart(response.cart);
   }
 
   useEffect(() => {
@@ -40,7 +54,12 @@ const App = () =>  {
           </Route>
 
           <Route exact path="/cart">
-            <Cart cart={cart} />
+            <Cart 
+              cart={cart}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+              handleEmptyCart={handleEmptyCart}
+            />
           </Route>
         </Switch>
       </div>
