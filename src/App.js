e09f5e8b3203c50222b6e1a84d70736a1533f18e
dipendradangol <dipendra.dangol11@gmail.com>
2021-03-16 00:24:08 +0200

    complete checkout using stripe and commerce.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import { Products, Navbar, Cart, Checkout } from './components';
 
 const App = () =>  {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState({})
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -38,6 +40,24 @@ const App = () =>  {
     setCart(response.cart);
   }
 
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  }
+
+  const handleCaptureCheckout  = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder)
+
+      setOrder(incomingOrder);
+      refreshCart();
+
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  }
+
   useEffect(() => {
     fetchProducts();
     fetchCart();
@@ -62,7 +82,12 @@ const App = () =>  {
             />
           </Route>
           <Route exact path="/checkout">
-            <Checkout cart={cart} />
+            <Checkout 
+              cart={cart}
+              order={order}
+              onCaptureCheckout={handleCaptureCheckout}
+              error={errorMessage}
+            />
           </Route>
         </Switch>
       </div>
